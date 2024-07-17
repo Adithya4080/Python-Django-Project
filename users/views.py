221d379953abcb4dcda83c566d23dac8e6e471ef
adithya4080 <adithyas712@gmail.com>
2024-07-17 11:22:21 +0530

    update

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login as auth_login
+from django.shortcuts import render, reverse
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.http.response import HttpResponseRedirect
 
 def login(request):
@@ -13,22 +13,17 @@ def login(request):
                 auth_login(request,user)
 
                 return HttpResponseRedirect("/")
-            else:
-                context={
-                    "title" : "Login",
-                    "error" : True,
-                    "message" : "Invalid username or password"
-                }
-                return render(request, "users/login.html", context=context)
-        else:
-            context={
-                "title" : "Login",
-                "error" : True,
-                "message" : "Invalid username or password"
-            }
-            return render(request, "users/login.html", context=context)
-
+        context={
+            "title" : "Login",
+            "error" : True,
+            "message" : "Invalid username or password"
+        }
+        return render(request, "users/login.html", context=context)        
     context = {
         "title": "Login"
     }
     return render(request, "users/login.html", context=context)
+
+def logout(request):
+    auth_logout(request)
+    return HttpResponseRedirect(reverse("web:index"))
