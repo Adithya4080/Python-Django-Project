ee734acd361a803ace9d8832fc5090ea1fc154c1
adithya4080 <adithyas712@gmail.com>
2024-07-18 16:51:17 +0530

    update

--- users/forms.py
+++ users/forms.py
@@ -5,3 +5,12 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ["first_name", "last_name", "email", "username", "password"]
+        widgets = {
+            "password" : forms.widgets.PasswordInput(
+                attrs={
+                    "class":"hello hi",
+                    "data-value":"123",
+                    "placeholder":"Enter Strong Password"
+                }
+            )
+        }
