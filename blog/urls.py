feac46747385a135dd37533576f6d2362881a073
adithya4080 <adithyas712@gmail.com>
2024-07-20 11:25:19 +0530

    update

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include("users.urls",namespace="users")),
-    path('', include("web.urls",namespace="web"))
+    path('', include("web.urls",namespace="web")),
+    path('posts/', include("posts.urls",namespace="posts"))
 ]
 
 
