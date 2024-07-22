0326dbbf4587b7ed4f12164ff9cf3a63a833f0e9
adithya4080 <adithyas712@gmail.com>
2024-07-22 17:07:28 +0530

    update

--- posts/forms.py
+++ posts/forms.py
@@ -6,3 +6,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         exclude = {'author', 'published_date', 'is_deleted', 'categories'}
+
+        widgets={
+            "time_to_read": forms.TextInput(attrs={'class':"input"}),
+            "title": forms.TextInput(attrs={'class':"input"}),
+            "short_description": forms.Textarea(attrs={'class':"input"}),
+        }
